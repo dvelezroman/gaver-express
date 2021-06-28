5ef572f7e0cd4a6ccb4c38c75eb1c2338fe53433
Dario Velez Roman <dvelezroman@gmail.com>
2021-06-27 21:23:39 -0300

    changes

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/ping', function(req, res, next) {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).json({ status: true, msg: 'Pong...' })
+})
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express powered by Plesk' });
 });
