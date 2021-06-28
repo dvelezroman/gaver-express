f5d99eb64540e1dac90dbc84b95b4ae1740415f8
Dario Velez Roman <dvelezroman@gmail.com>
2021-06-27 21:39:42 -0300

    add twilio client

---
+++ controllers/WSBot.js
@@ -0,0 +1,35 @@
+import dotenv from 'dotenv';
+import twilio from 'twilio';
+dotenv.config();
+
+const {
+  SID: accountSid,
+  KEY: TwilloAuthToken
+} = process.env;
+
+twilio(accountSid, TwilloAuthToken);
+const { MessagingResponse } = twilio.twiml;
+const customsearch = google.customsearch('v1');
+
+/**
+ * @class WhatsappBot
+ * @description class will implement bot functionality
+ */
+class WhatsappBot {
+
+  static async botMain(req, res, next) {
+    const twiml = new MessagingResponse();
+    const q = req.body.Body;
+    try {
+      twiml.message(`Hello I am whatsapp BOT..., you sent this: ${q}`);
+
+      res.set('Content-Type', 'text/xml');
+
+      return res.status(200).send(twiml.toString());
+    } catch (error) {
+      return next(error);
+    }
+  }
+}
+
+export default WhatsappBot;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 var express = require('express');
+const { default: WhatsappBot } = require('../controllers/WSBot');
 var router = express.Router();
 
-/* GET home page. */
+// GET test json page
 router.get('/ping', function(req, res, next) {
   res.setHeader('Content-Type', 'application/json');
   res.status(200).json({ status: true, msg: 'Pong...' })
 })
+// POST to twilio client
+router.get('/ws', WhatsappBot.botMain)
+
+/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express powered by Plesk' });
 });
